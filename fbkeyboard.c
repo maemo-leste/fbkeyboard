8631a81687164e69afeae854a766f0cdaee9236f
uvos <devnull@uvos.xyz>
2021-05-03 22:13:32 +0200

    use poll() instead of litteraly polling

--- fbkeyboard.c
+++ fbkeyboard.c
@@ -31,6 +31,7 @@ Copyright 2021 Carl Philipp Klemm <carl@uvos.xyz>
 #include <libinput.h>
 #include <errno.h>
 #include <libudev.h>
+#include <sys/poll.h>
 
 #include "layout.h"
 #include "draw.h"
@@ -74,7 +75,6 @@ int main(int argc, char *argv[])
 	if (!init_uinput())
 		return -2;
 
-	
 	struct udev *udev_cntxt = udev_new();
 	struct libinput *li;
 	struct libinput_event *event = NULL;
@@ -85,7 +85,9 @@ int main(int argc, char *argv[])
 		return -4;
 	
 	libinput_udev_assign_seat(li, "seat0");
-	
+	int lifd = libinput_get_fd(li);
+	if (lifd < 0)
+		return -5;
 
 	char *backbuffer = malloc(line_length * hight);
 	if (backbuffer == 0) {
@@ -96,20 +98,25 @@ int main(int argc, char *argv[])
 	fill_rect(backbuffer, 0, 0, xres, hight, 0x444444);
 	
 	int pressed = -1;
+	
+	draw_keyboard(backbuffer, xres, hight, pressed, layoutuse);
+	lseek(fbfd, line_length * (yres - hight), SEEK_SET);
+	write(fbfd, backbuffer, line_length * hight);
 
 	while (true) {
-		draw_keyboard(backbuffer, xres, hight, pressed, layoutuse);
-		lseek(fbfd, line_length * (yres - hight), SEEK_SET);
-		write(fbfd, backbuffer, line_length * hight);
 		
-
-		int absolute_x = 0;
-		int absolute_y = 0;
-		bool touchdown = false;
-		bool released = false;
+		struct pollfd fds;
+		fds.fd = lifd;
+		fds.events = POLLIN;
+	        fds.revents = 0;
+		poll(&fds, 1, -1);
 		libinput_dispatch(li);
-		event = libinput_get_event(li);
-		if (event) {
+		while((event = libinput_get_event(li)))
+		{
+			int absolute_x = 0;
+			int absolute_y = 0;
+			bool touchdown = false;
+			bool released = false;
 			if (libinput_event_get_type(event) == LIBINPUT_EVENT_TOUCH_DOWN) {
 				touchdown = true;
 				struct libinput_event_touch *touch = libinput_event_get_touch_event(event);
@@ -121,61 +128,63 @@ int main(int argc, char *argv[])
 			 else if(libinput_event_get_type(event) == LIBINPUT_EVENT_TOUCH_UP) {
 				 released = true;
 			 }
-		}
-
-		if (released && pressed != -1) {
-			if (pressed <= 27)	//normal keys (abc...)
-				uinput_send_key(keys[layoutuse >> 1][pressed]);
-			else if (pressed == 28) {
-				layoutuse ^= 1;
-				uinput_send_modifier(KEY_LEFTCTRL,  layoutuse & 1);
+			if (released && pressed != -1) {
+				if (pressed <= 27)	//normal keys (abc...)
+					uinput_send_key(keys[layoutuse >> 1][pressed]);
+				else if (pressed == 28) {
+					layoutuse ^= 1;
+					uinput_send_modifier(KEY_LEFTCTRL,  layoutuse & 1);
+				}
+				else if (pressed == 29)
+					uinput_send_modifier(KEY_LEFTCTRL, 1);
+				else if (pressed <= 38)	//special keys (space, tab, ...)
+					uinput_send_key(keys[2][pressed - 28]);
+				else if (pressed == 39)	//second page
+					layoutuse ^= 2;
+				
+				pressed = -1;
 			}
-			else if (pressed == 29)
-				uinput_send_modifier(KEY_LEFTCTRL, 1);
-			else if (pressed <= 38)	//special keys (space, tab, ...)
-				uinput_send_key(keys[2][pressed - 28]);
-			else if (pressed == 39)	//second page
-				layoutuse ^= 2;
+
 			
-			pressed = -1;
-		}
-
-		
-		if (touchdown) {
-			switch (((absolute_y - yres) * yres +
-				 yres * hight) / (hight/5) / yres) {
-			case 0:
-				pressed = 29 + absolute_x * 7 / xres;
-				break;
-			case 1:
-				pressed = absolute_x * 10 / xres;
-				break;
-			case 2:
-				if (absolute_x > xres / 20 && absolute_x < xres * 19 / 20)
-					pressed = 10 + (absolute_x * 10 - xres / 2) / xres;
-				break;
-			case 3:
-				if (absolute_x < xres * 3 / 20)
-					pressed = 28;	//shift
-				else if (absolute_x < xres * 17 / 20)
-					pressed = 19 + (absolute_x * 10 - xres * 3 / 2) / xres;
-				else
-					pressed = 38;	//bksp
-				break;
-			case 4:
-				if (absolute_x < xres * 3 / 20)
-					pressed = 39;	//123
-				else if (absolute_x < xres * 5 / 20)
-					pressed = 26;
-				else if (absolute_x < xres * 15 / 20)
-					pressed = 36;	//space
-				else if (absolute_x < xres * 17 / 20)
-					pressed = 27;
-				else
-					pressed = 37;	//enter
-				break;
+			if (touchdown) {
+				switch (((absolute_y - yres) * yres +
+					 yres * hight) / (hight/5) / yres) {
+				case 0:
+					pressed = 29 + absolute_x * 7 / xres;
+					break;
+				case 1:
+					pressed = absolute_x * 10 / xres;
+					break;
+				case 2:
+					if (absolute_x > xres / 20 && absolute_x < xres * 19 / 20)
+						pressed = 10 + (absolute_x * 10 - xres / 2) / xres;
+					break;
+				case 3:
+					if (absolute_x < xres * 3 / 20)
+						pressed = 28;	//shift
+					else if (absolute_x < xres * 17 / 20)
+						pressed = 19 + (absolute_x * 10 - xres * 3 / 2) / xres;
+					else
+						pressed = 38;	//bksp
+					break;
+				case 4:
+					if (absolute_x < xres * 3 / 20)
+						pressed = 39;	//123
+					else if (absolute_x < xres * 5 / 20)
+						pressed = 26;
+					else if (absolute_x < xres * 15 / 20)
+						pressed = 36;	//space
+					else if (absolute_x < xres * 17 / 20)
+						pressed = 27;
+					else
+						pressed = 37;	//enter
+					break;
+				}
 			}
 		}
+		draw_keyboard(backbuffer, xres, hight, pressed, layoutuse);
+		lseek(fbfd, line_length * (yres - hight), SEEK_SET);
+		write(fbfd, backbuffer, line_length * hight);
 	}
 	
 	libinput_unref(li);
